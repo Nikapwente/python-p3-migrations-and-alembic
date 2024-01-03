0e03c42a20f0dc38768979569262ea95325e6e2e
Nikapwente <hrsmajor@gmail.com>
2024-01-03 14:10:53 +0300

    Assignment Complete

---
+++ app/migrations/versions/b78038600e59_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: b78038600e59
+Revises: 
+Create Date: 2024-01-03 14:02:44.165391
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'b78038600e59'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
